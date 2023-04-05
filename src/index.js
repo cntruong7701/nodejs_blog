c97c8927911d92333dbb88da524a8bc4289ff5ab
cntruong7701 <cntruong12c5nct@gmail.com>
2023-04-05 15:43:20 +0700

    05/04/2023

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require ('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine(
+  {
+    extname: 'hbs'
+  }
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log('PATH:',path.join(__dirname, 'resources/views'));
+
+app.get('/', (req,res) => {
+  res.render('home');
+})
+app.get('/chi-tiet', (req,res) => {
+  res.render('news');
+})
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`),
+);
