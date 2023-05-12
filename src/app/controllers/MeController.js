65834f6a997ba0065f310b24b5f408c9690e9314
cntruong7701 <cntruong12c5nct@gmail.com>
2023-05-12 17:23:34 +0700

    add-icon

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,10 +4,20 @@ const { multipleMongooseToOject } = require('../../util/mongoose');
 class MeController {
     //[GET] /me/stored/courses
     storeCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses',{
-                courses: multipleMongooseToOject(courses)
-            }))
+
+        const courseQuery = Course.find({});
+
+        if (req.query.hasOwnProperty('_sort')) {
+            res.json({message: 'thanhcoong'});
+        }
+
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
+            .then(([courses, deleteCount]) => 
+                res.render('me/stored-courses', {
+                    deleteCount,
+                    courses: multipleMongooseToOject(courses),
+                })
+            )
             .catch(next);
     }
 
